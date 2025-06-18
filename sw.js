39e07712ec75fd408e2b419712c0c1bdaa4438a2
Ache-u <acheu@live.com>
2025-06-18 14:15:48 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,41 @@
+const CACHE_NAME = 'minos-cache-v1';
 const ASSETS = [
-  '/flipminos/',
   '/flipminos/index.html',
   '/flipminos/manifest.json',
   '/flipminos/libs/page-flip.min.css',
   '/flipminos/libs/page-flip.browser.min.js',
   '/flipminos/img/minos1.png',
-  // ...etc
+  // ...
+  '/flipminos/img/minos10.png'
 ];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(res => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, res.clone());
+          return res;
+        });
+      });
+    })
+  );
+});
